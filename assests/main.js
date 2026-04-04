91054fb49af7bcba256cb3b62c6c8331371d8551
NisaNajafli <ncfliniss@gmail.com>
2026-04-05 03:26:56 +0400

    new

--- assests/main.js
+++ assests/main.js
@@ -44,3 +44,47 @@ window.addEventListener("scroll", () => {
 
   lastScrollY = currentY;
 });
+
+const modal = document.querySelector(".modal");
+const openBtns = document.querySelectorAll(".open_modal");
+const closeBtn = document.querySelector(".modal_close");
+const overlay = document.querySelector(".modal_overlay");
+
+// OPEN
+openBtns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    e.preventDefault();
+    modal.classList.add("active");
+  });
+});
+
+// CLOSE
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+const toggle = document.querySelector(".mobile_toggle");
+const menu = document.querySelector(".mobile_menu");
+const closeBtn2 = document.querySelector(".mobile_close");
+const overlay2 = document.querySelector(".mobile_overlay");
+
+// OPEN
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+// CLOSE
+closeBtn2.addEventListener("click", () => {
+  toggle.classList.remove("active");
+  menu.classList.remove("active");
+});
+
+overlay2.addEventListener("click", () => {
+  toggle.classList.remove("active");
+  menu.classList.remove("active");
+});
